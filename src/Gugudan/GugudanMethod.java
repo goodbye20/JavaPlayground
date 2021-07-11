d865ffd42ae01a96952ca0844a9d28b86808adf2
Reload0213 <85924448+goodbye20@users.noreply.github.com>
2021-07-11 16:32:04 +0900

    method test and arror fix

---
+++ src/Gugudan/GugudanMethod.java
@@ -0,0 +1,23 @@
+package Gugudan;
+
+public class GugudanMethod {
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+	
+		
+		result[i] =  times * (i+1);
+		
+		}
+		return result;
+	}
+	
+		
+		
+	public static void main(String[] args) {
+			int[] result = calculate(2);
+			for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+	}
+	}
+}
