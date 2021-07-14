a292030d31d97416bee85e94508184d822f991fa
Reload0213 <85924448+goodbye20@users.noreply.github.com>
2021-07-14 16:04:36 +0900

    Calender print add year and leapyears

--- src/Calender/CalenderEx.java
+++ src/Calender/CalenderEx.java
@@ -3,11 +3,17 @@ package Calender;
 import java.util.Scanner;
 
 public class CalenderEx {
-	private final static String PROMPT = "cal> ";
+	private final static String PROMPT1 = "Year> ";
+	private final static String PROMPT2 = "Month> ";
 	static int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	static int[] leapMaxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public static int getMaxDaysOfMonth(int month) {
+	public static int getMaxDaysOfMonth(int year, int month) {
 		return maxDays[month - 1];
+	}
+
+	public static int getLeapMaxDaysOfMonth(int year, int month) {
+		return leapMaxDays[month - 1];
 	}
 
 	public static void main(String[] args) {
@@ -16,28 +22,41 @@ public class CalenderEx {
 		Scanner scanner = new Scanner(System.in);
 
 		while (true) {
+			System.out.println("년도를 입력하세요");
+			int year = scanner.nextInt();
+			System.out.println(PROMPT1 + year);
 			System.out.println("달을 입력하세요.");
-
 			int month = scanner.nextInt();
-			System.out.println(PROMPT + month);
+			System.out.println(PROMPT2 + month);
 			if (month == -1) {
 				break;
 			} else if (month > 12) {
 				continue;
 			}
-			System.out.println("      2021년  " + month + "월");
+			System.out.println("      "+year+"년  " + month + "월");
 			System.out.println(" SU MO TO WE TH FR SA");
 			System.out.println("=====================");
-			int m1 = getMaxDaysOfMonth(month);
-
-			for (int i = 1; i <= m1; i++) {
+			int m1 = getMaxDaysOfMonth(year,month);
+			int m2 = getLeapMaxDaysOfMonth(year, month);
+			if(year % 4 == 0 && (year % 100 !=0 || year % 400 == 0)) {
+			for (int i = 1; i <= m2; i++) {
 				System.out.printf("%3d", i);
 				if (i % 7 == 0) {
 					System.out.println();
 				}
 			}
 			System.out.println();
+			} else {
+				for (int i = 1; i <= m1; i++) {
+					System.out.printf("%3d", i);
+					if (i % 7 == 0) {
+						System.out.println();
+			}
 		}
+			System.out.println();
+		}
+		
+	}
 		System.out.println("완료.");
 	}
 }
